48df3782a29e18dd92ae66ebfe805c38df2f23dc
Claus Guttesen <kometen@gmail.com>
2017-07-14 14:33:30 +0200

    Initial Commit

---
+++ BloomFilter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BloomFilter
+//
+//  Created by Claus Guttesen on 14.07.2017.
+//  Copyright © 2017 Claus Guttesen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
