a7306caf5827fe2bca77eff143e490533229d461
Claus Guttesen <kometen@gmail.com>
2017-07-16 00:06:31 +0200

    Moved test-elements to vector.

--- BloomFilter/main.cpp
+++ BloomFilter/main.cpp
@@ -19,20 +19,18 @@ const auto ELEMENTS = 1800000;
 const auto FALSE_POSITIVES = 0.02;
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Jeg æder blåbærsyltetøj!" << std::endl;
-    
     auto filter_size = -1 * (ELEMENTS * log(FALSE_POSITIVES)) / (log(2.0) * log(2.0));
     auto hash_functions = filter_size / ELEMENTS * log(2.0);
 
-    //struct timespec start, finish;
-    
+    std::cout << "Elements: " << ELEMENTS << ", false positives: " << FALSE_POSITIVES << std::endl;
+    std::cout << "filter size: " << filter_size << ", hash functions: " << hash_functions << std::endl;
+
     BloomFilter::BloomFilter bf(filter_size, hash_functions);
 
     auto present = false;
-    auto s1 = "5";
-    auto s2 = "101";
-    auto s3 = "987654";
-    auto s4 = "1987654";
+    std::vector<std::string> verify_elements = {
+        "5", "101", "987654", "1987654", "Claus", "1799999"
+    };
 
     std::vector<std::string> v;
     v.reserve(ELEMENTS);
@@ -44,7 +42,6 @@ int main(int argc, const char * argv[]) {
     }
 
     std::clock_t begin = clock();
-    //clock_gettime(CLOCK_MONOTONIC, &start);
     
     for (auto i : v) {
         bf.add(reinterpret_cast<const uint8_t *>(i.c_str()), i.length());
@@ -52,40 +49,16 @@ int main(int argc, const char * argv[]) {
 
     std::clock_t end = clock();
     auto elapsed = double(end - begin);
-    //clock_gettime(CLOCK_MONOTONIC, &finish);
-    //auto elapsed = (finish.tv_nsec - start.tv_nsec);
     std::cout << "Time spent inserting into Bloom filter: " << elapsed << std::endl;
 
-    present = bf.possiblyContains(reinterpret_cast<const uint8_t *>(s1), strlen(s1));
-    
-    if (present) {
-        std::cout << "Teksten '" << s1 << "' findes." << std::endl;
-    } else {
-        std::cout << "Teksten '" << s1 << "' findes ikke." << std::endl;
-    }
+    for (auto i : verify_elements) {
+        present = bf.possiblyContains(reinterpret_cast<const uint8_t *>(i.c_str()), i.size());
 
-    present = bf.possiblyContains(reinterpret_cast<const uint8_t*>(s2), strlen(s2));
-    
-    if (present) {
-        std::cout << "Teksten '" << s2 << "' findes." << std::endl;
-    } else {
-        std::cout << "Teksten '" << s2 << "' findes ikke." << std::endl;
-    }
-
-    present = bf.possiblyContains(reinterpret_cast<const uint8_t*>(s3), strlen(s3));
-    
-    if (present) {
-        std::cout << "Teksten '" << s3 << "' findes." << std::endl;
-    } else {
-        std::cout << "Teksten '" << s3 << "' findes ikke." << std::endl;
-    }
-    
-    present = bf.possiblyContains(reinterpret_cast<const uint8_t*>(s4), strlen(s4));
-    
-    if (present) {
-        std::cout << "Teksten '" << s4 << "' findes." << std::endl;
-    } else {
-        std::cout << "Teksten '" << s4 << "' findes ikke." << std::endl;
+        if (present) {
+            std::cout << "Element '" << i << "' is (probably) present." << std::endl;
+        } else {
+            std::cout << "Element '" << i << "' is not present." << std::endl;
+        }
     }
     
     return 0;
